9d025900db1e8718c010c5845df5a20cd44b61b5
aericpp <p.yue89@gmail.com>
2023-10-16 14:26:33 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ cd "$CURRENT_PATH/nginx"
 mv auto/configure configure
 patch -p1 <../ngx_http_proxy_connect_module/patch/proxy_connect_rewrite_102101.patch
 
+make clean
 ./configure \
     --prefix=/usr/share/nginx \
     --sbin-path=/usr/sbin/nginx \
@@ -45,9 +46,12 @@ patch -p1 <../ngx_http_proxy_connect_module/patch/proxy_connect_rewrite_102101.p
     # --with-ld-opt=-static
 make
 
+cp -r ${CURRENT_PATH}/nginx_debian ${CURRENT_PATH}/nginx_debian_${1}
+
 # get execute file
-test -d ${CURRENT_PATH}/nginx_debian/usr/sbin/ || mkdir -p ${CURRENT_PATH}/nginx_debian/usr/sbin/
-cp ${CURRENT_PATH}/nginx/objs/nginx ${CURRENT_PATH}/nginx_debian/usr/sbin/nginx
+test -d ${CURRENT_PATH}/nginx_debian/usr/sbin/ || mkdir -p ${CURRENT_PATH}/nginx_debian_${1}/usr/sbin/
+cp ${CURRENT_PATH}/nginx/objs/nginx ${CURRENT_PATH}/nginx_debian_${1}/usr/sbin/nginx
+cp ${CURRENT_PATH}/nginx/objs/nginx.8 ${CURRENT_PATH}/nginx_debian_${1}/usr/sbin/nginx.8
 
 NGINX_VERSION_NUMBER=$(cat ${CURRENT_PATH}/nginx.version.number)
 OPENSSL_VERSION=$(cat ${CURRENT_PATH}/openssl.version)
@@ -56,18 +60,18 @@ echo "[++] nginx_${NGINX_VERSION_NUMBER}.openssl_${OPENSSL_VERSION}.debian+${1}.
 
 # make deb package
 cd ${CURRENT_PATH}
-NG_PKG_SIZE=`du -sk nginx_debian|awk '{print $1}'`
+NG_PKG_SIZE=`du -sk nginx_debian_${1}|awk '{print $1}'`
 NG_PKG_VERSION=${NGINX_VERSION_NUMBER}
-test -d "nginx_debian/DEBIAN" || mkdir -p "nginx_debian/DEBIAN" 
-sed -e "s|%%SIZE%%|${NG_PKG_SIZE}|" -e "s|%%VERSION%%|${NGINX_VERSION_NUMBER}|" < control_tmpl > nginx_debian/DEBIAN/control
-test -d "nginx_debian/var/lib/nginx" || mkdir -p "nginx_debian/var/lib/nginx"        
-test -d "nginx_debian/var/log/nginx" || mkdir -p "nginx_debian/var/log/nginx"
-test -d "nginx_debian/var/www/html" || mkdir -p "nginx_debian/var/www/html"
-test -d "nginx_debian/etc/nginx/modules-available" || mkdir -p "nginx_debian/etc/nginx/modules-available"
-test -d "nginx_debian/etc/nginx/modules-enabled" || mkdir -p "nginx_debian/etc/nginx/modules-enabled"
-test -d "nginx_debian/etc/nginx/conf.d" || mkdir -p "nginx_debian/etc/nginx/conf.d"
-test -d "nginx_debian/etc/nginx/sites-enabled" || mkdir -p "nginx_debian/etc/nginx/sites-enabled"
+test -d "nginx_debian_${1}/DEBIAN" || mkdir -p "nginx_debian_${1}/DEBIAN" 
+sed -e "s|%%SIZE%%|${NG_PKG_SIZE}|" -e "s|%%VERSION%%|${NGINX_VERSION_NUMBER}|" < control_tmpl > nginx_debian_${1}/DEBIAN/control
+test -d "nginx_debian_${1}/var/lib/nginx" || mkdir -p "nginx_debian_${1}/var/lib/nginx"        
+test -d "nginx_debian_${1}/var/log/nginx" || mkdir -p "nginx_debian_${1}/var/log/nginx"
+test -d "nginx_debian_${1}/var/www/html" || mkdir -p "nginx_debian_${1}/var/www/html"
+test -d "nginx_debian_${1}/etc/nginx/modules-available" || mkdir -p "nginx_debian_${1}/etc/nginx/modules-available"
+test -d "nginx_debian_${1}/etc/nginx/modules-enabled" || mkdir -p "nginx_debian_${1}/etc/nginx/modules-enabled"
+test -d "nginx_debian_${1}/etc/nginx/conf.d" || mkdir -p "nginx_debian_${1}/etc/nginx/conf.d"
+test -d "nginx_debian_${1}/etc/nginx/sites-enabled" || mkdir -p "nginx_debian_${1}/etc/nginx/sites-enabled"
 
-dpkg -b nginx_debian "nginx_${NGINX_VERSION_NUMBER}.openssl_${OPENSSL_VERSION}.debian+${1}.amd64.deb"
-mkdir "${CURRENT_PATH}/release"
-cp "nginx_${NGINX_VERSION_NUMBER}.openssl_${OPENSSL_VERSION}.debian+${1}.amd64.deb" "${CURRENT_PATH}/release"
+dpkg -b "nginx_debian_${1}" "nginx_${NGINX_VERSION_NUMBER}.openssl_${OPENSSL_VERSION}.debian+${1}.amd64.deb"
+test -d "${CURRENT_PATH}/release" || mkdir "${CURRENT_PATH}/release"
+cp "nginx_${NGINX_VERSION_NUMBER}.openssl_${OPENSSL_VERSION}.debian+${1}.amd64.deb" "${CURRENT_PATH}/release/"
