3ac250bbc9e2abc3d6db6f2b1355189e257c8ade
aericpp <p.yue89@gmail.com>
2023-10-22 20:53:47 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,10 +33,12 @@ test -d ${CURRENT_PATH}/nginx/objs && make clean
     --without-http_memcached_module \
     --without-http_uwsgi_module \
     --without-http_scgi_module \
+    --with-http_auth_basic_module \
     --with-http_gzip_static_module \
     --with-http_ssl_module \
     --with-http_stub_status_module \
     --with-http_v2_module \
+    --with-http_v3_module \
     --with-openssl="${CURRENT_PATH}/openssl" \
     --with-stream \
     --with-stream_ssl_module \
